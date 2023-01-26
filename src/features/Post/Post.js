79a9fe08674b454a5e60cd0d5ba1cf8998e86e9f
Stefano Calo <stefano.cal92@gmail.com>
2023-01-26 11:16:48 +0100

    Update loading content condition

--- src/features/Post/Post.js
+++ src/features/Post/Post.js
@@ -233,7 +233,7 @@ export const Post = ({post, index, onToggleComment}) => {
                         role='button'
                         className="showMoreButton">{isShortened ? 'Show more...' : 'Show less...'}</div>
                     </div>}
-                {(!post.is_gallery && post.url && post.thumbnail !== null && !post.selftext) && 
+                {(!post.is_gallery && post.url && post.thumbnail !== null && !post.selftext && post.post_hint !== 'link') && 
                     <div className="imageContainer">
                         <Image url={post.url} thumbnail={post.thumbnail} />
                     </div>
